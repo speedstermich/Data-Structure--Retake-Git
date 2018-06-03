18080437c8915ea18ec4736a0242f4383b8811f5
zeus <michael.201096@gmail.com>
2018-06-03 14:50:45 +0800

    Update

--- Homework/bst_1.c
+++ Homework/bst_1.c
@@ -1,7 +1,7 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #define MAXSIZE 100
-
 struct BST{
 	int key;
 	struct BST *left, *right;
--- Homework/find.c
+++ Homework/find.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
+#include <ctype.h>
 
 #define MAXWORD 32
-
+#define MAXSIZE 1024
 struct BSTnode{
 	char word[MAXWORD];
 	struct BSTnode *right, *left;
 };
 
+char GetWord(FILE *fp, char *buf)
+{
+	int c = 0;
+	while( (c = fgetc(fp) ) != EOF && !isalpha(c) )
+	{}
+	if(c == EOF)
+	{
+		return 0;
+	}
+	while ( isalpha(c) )
+	{
+		*buf = tolower(c);
+		++buf;
+		c = fgetc(fp);
+	}
+	*buf = 0;
+	return 1;
+}
 
 int main(int argc, char const *argv[]) {
 	FILE *fp;
-
+	char word[MAXWORD];
 	if((fp = fopen("in.txt", "r")) == NULL)
 	{
 		fprintf(stderr, "File Doesn't Exist !!\n");
 		return -1;
 	}
 
-	/*while()
+	while(GetWord(fp, word))
 	{
 
 	}*/
