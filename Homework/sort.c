9eb11e51662a1a40e4e0004274f72ea030583af4
Zeus-PC <michael.201096@gmail.com>
2018-06-06 05:02:23 +0800

    Update for sort.c for test case 1 already true

--- Homework/sort.c
+++ Homework/sort.c
@@ -8,6 +8,7 @@ void swap(int* a, int* b)
     int t = *a;
     *a = *b;
     *b = t;
+   // ++count;
 }
 
 void QuickSort(int k[ ], int left, int right)
@@ -17,36 +18,45 @@ void QuickSort(int k[ ], int left, int right)
 	{
 		last=left;
 		for(i=left+1;i<=right;i++)
-			if(k[i]<k[left])
+			if( k[i]<k[left] )
 				swap(&k[++last], &k[i]);
-				//count++;
 			swap(&k[left], &k[last]);
 		QuickSort(k,left,last-1);
 		QuickSort(k,last+1,right);
 	}
 }
 
-/*void BubbleSort()
+void BubbleSort(int arr[], int n)
 {
+	int i, j;
+	for(i = 0; i < n-1; i++)
+		for(j = 0; j < n -1; j++)
+			if( arr[j] > arr[j + 1] )
+			{
+				++count;
+				swap( &arr[j], &arr[j + 1] );
+			}
+}
 
-}*/
-
-void selectionSort(int arr[], int n)
+void SelectionSort(int arr[], int n)
 {
-    int i, j, min_idx;
+	int i, j, min_idx;
 
     // One by one move boundary of unsorted subarray
-    for (i = 0; i < n-1; i++)
-    {
+    	for (i = 0; i < n-1; i++)
+    	{
         // Find the minimum element in unsorted array
-        min_idx = i;
-        for (j = i+1; j < n; j++)
-          if (arr[j] < arr[min_idx])
-            min_idx = j;
-
-        // Swap the found minimum element with the first element
-        swap(&arr[min_idx], &arr[i]);
-    }
+		min_idx = i;
+		for (j = i+1; j < n; j++)
+			++count;
+			if (arr[j] < arr[min_idx])
+			{
+				min_idx = j;
+			}
+	// Swap the found minimum element with the first element
+		swap(&arr[min_idx], &arr[i]);
+		//++count;
+	}
 }
 
 void merge(int x[ ],int tmp[ ],int left,int leftend,int rightend)
@@ -101,20 +111,33 @@ int main(int argc, char const *argv[]) {
 	scanf("%d %d", &number, &choose);
 	for(i = 0; i < number; i++)
 	{
-		scanf("%d ", &array[i]);
+		scanf("%d", &array[i]);
 	}
-	if(choose == 1)
+	if(choose == 1) //true
 	{
-		QuickSort(array, 0, number - 1);
+		SelectionSort(array, number);
 		PrintArray(array, 0, number);
-		printf("%d", count);
+		printf("%d\n", count );
 	}
 	else if(choose == 2)
+	{
+		BubbleSort(array, number);
+		PrintArray(array, 0, number);
+		printf("%d\n", count );
+	}
+	else if(choose == 3)
 	{
 		Adjust(array, 0, number - 1);
 		PrintArray(array, 0, number);
 		printf("%d", count);
 	}
+	else if(choose == 5)
+	{
+		QuickSort(array, 0, number - 1);
+		PrintArray(array, 0, number);
+		printf("%d\n", count);
+	}
+
 
 	return 0;
 }
