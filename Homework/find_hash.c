9d00f2920ba81fc5f9a89b3e445279f506d44ce2
speesdtermich <michael.201096@gmail.com>
2018-06-14 13:35:07 +0800

    New update for find_hash.c

--- Homework/find_hash.c
+++ Homework/find_hash.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAXWORD 20
+int count;
+
+int LinearSearch(char key[], int n, char k)
+{
+
+}
+
+int main(int argc, char const *argv[]) {
+	int method;
+	char key[MAXWORD];
+	FILE *fp;
+
+	//scanf()
+	return 0;
+}
