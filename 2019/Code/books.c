be49c2ed6232292f646624d93f1d7e2f9906ed21
Script Kiddie <michael.201096@gmail.com>
2019-03-24 06:38:17 +0800

    update

---
+++ 2019/Code/books.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct books{
+	char name[50];
+	char author[20];
+	char press[30];
+	char year[10];
+};
+
+int main(int argc, char const *argv[]) {
+	int op, i, j, n, clear = 0; //variable declaration
+	int total_line = 0; //total line number
+	char temp_str[200];
+	char search_tmp[200];
+	struct books info[500]; //books List
+
+	return 0;
+}
