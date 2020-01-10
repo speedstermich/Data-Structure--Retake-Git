f089da0c0d03df8b7facd29414b26c03e6be0fea
Michael Octavianus Korintus <michael.201096@gmail.com>
2020-01-11 04:49:13 +0800

    Create .atom-beautify.encode.c

---
+++ 2019/Code/.atom-beautify.encode.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+	int input, arr[100000];
+	int i;
+	scanf("%d", &input);
+	for (i = 0; i < input; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+
+	return 0;
+}
